00336354c0b5335d6fda1d1e388094903c96d5cd
danishtahir12 <danishtahir12.docs@gmail.com>
2022-06-13 16:59:08 +0530

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,6 +1,7 @@
 <?php 
 echo "Welcome to Xcempt Media";
 echo "Its day5";
-echo "Learn about API in PHP";
+echo "Learn about API in PHP & Laravel.";
 echo "I am going to start now."
+echo "First day has about to end."
 ?>
